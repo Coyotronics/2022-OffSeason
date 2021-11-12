d5303fdd8ce7145dead557265ce5f32355156451
Hrishikesh-S <hrishikeshs2005@gmail.com>
2021-11-11 23:28:22 -0600

    This is my submission for practice assignment A1A.

---
+++ HrishikeshSrirangam - A1A.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class HrishikeshS 
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[5];
+        for (int i = 0; i < numbers.length; i++) 
+            numbers[i] = scanner.nextInt();
+        for (int i = numbers.length - 1; i >= 0; i--)
+            System.out.print(numbers[i] + " "); 
+    }
+}
