c7161fb3eb2b1fbe62bd4ec5e367f377cc8a0683
RiktheKing <45601021+RiktheKing@users.noreply.github.com>
2021-11-12 13:56:14 -0600

    Update GitHubtest.java

--- GitHubtest.java
+++ GitHubtest.java
@@ -1,4 +1,4 @@
-public class GitHubtest {
+public class GitHubtest {//Ricky Made this
     public static void main(String[] args) {
         System.out.println("Grass is made up by those who dare take a step outside their door. They created this lie to make it seem like the outside is more than just an empty void. We programmers, anime watchers, Redditors, etc need to spread the real truth. The real truth of what is beyond our door. Facebook, Twitter, etc are all lies. Reddit is the only true and divine platform. I was once an atheist but once I found Reddit, I knew Reddit was the one and only genuine god. Reddit gave me meaning and purpose. But more than that, it taught me the truth. So now you enlightened souls, spread the truth about grass.");
     }
